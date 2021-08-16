ffc7763066cfdaddfb8115e13712de93d4af5548
Ryan Irilli <ryanirilli@gmail.com>
2021-08-16 07:00:16 -0700

    fade in top nav

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { Box, Flex, Text, Icon, IconButton, HStack } from "@chakra-ui/react";
 import { useContext } from "react";
 import { FiLinkedin, FiTwitter, FiGithub } from "react-icons/fi";
@@ -21,6 +22,21 @@ const icons = [
   },
 ];
 
+const fadeIn = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 1,
+      duration: 1,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    y: 10,
+  },
+};
+
 export default function TopNav(): JSX.Element {
   const { colors } = useContext(BackgroundContext);
   return (
@@ -31,26 +47,28 @@ export default function TopNav(): JSX.Element {
       bg={colors.bg}
       transition="background 500ms ease"
     >
-      <Box px={[4, null, 8]} py={[2, null, 4]}>
-        <Flex>
-          <Box pr={4} flexGrow={[1, null, 0]}>
-            <Text textStyle="h3">Ryan Irilli</Text>
-          </Box>
-          <HStack>
-            {icons.map(({ DisplayIcon, label, url }, i) => (
-              <IconButton
-                key={i}
-                isRound
-                variant="outline"
-                borderColor={colors.color}
-                aria-label={label}
-                icon={<Icon as={DisplayIcon} />}
-                onClick={() => window.open(url)}
-              />
-            ))}
-          </HStack>
-        </Flex>
-      </Box>
+      <motion.div initial="hidden" animate="visible" variants={fadeIn}>
+        <Box px={[4, null, 8]} py={[2, null, 4]}>
+          <Flex>
+            <Box pr={4} flexGrow={[1, null, 0]}>
+              <Text textStyle="h3">Ryan Irilli</Text>
+            </Box>
+            <HStack>
+              {icons.map(({ DisplayIcon, label, url }, i) => (
+                <IconButton
+                  key={i}
+                  isRound
+                  variant="outline"
+                  borderColor={colors.color}
+                  aria-label={label}
+                  icon={<Icon as={DisplayIcon} />}
+                  onClick={() => window.open(url)}
+                />
+              ))}
+            </HStack>
+          </Flex>
+        </Box>
+      </motion.div>
     </Box>
   );
 }
