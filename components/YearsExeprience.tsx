3c6f98709aa9646b9b89d5a1c6f4521e3b68e595
Ryan Irilli <ryanirilli@gmail.com>
2021-09-27 12:01:32 -0700

    update copy

--- components/YearsExeprience.tsx
+++ components/YearsExeprience.tsx
@@ -57,9 +57,8 @@ export default function YearsExperience() {
               <SectionHeading>10+ Years Experience</SectionHeading>
               <motion.div variants={fadeIn}>
                 <Text maxW={80}>
-                  I have help build applications that scale and adapt to the
-                  business, by using tools and frameworks made for modern web
-                  development.
+                  I have helped build applications at scale and by using tools
+                  and frameworks made for modern web development.
                 </Text>
               </motion.div>
             </motion.div>
